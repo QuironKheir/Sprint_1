184c271a7fecd17fc076779cdcc617f678c7f235
aenrique <enriquezjimenez.andres@gmail.com>
2022-07-19 13:18:18 +0200

    Ejercicio 1

---
+++ exercici.js
@@ -0,0 +1,12 @@
+
+// Exercici 1
+// Crea una funció que mostri per consola el nom d'usuari/ària 
+// en invocar-la passant-li el nom com a paràmetre.
+
+var user_var = 'Andrés Jonathan Enríquez Jiménez';
+
+function read_consol(name){
+    console.log(name);
+}
+
+read_consol(user_var);
